f374735fbcf20bd7c75aad0a61d45f27eb10ed8c
Crost Dev <dev@crost.com>
2026-06-28 00:06:11 +0200

    fix: remove ShineBorder usage from Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,10 +1,5 @@
 <script>
   import Hero from "../components/Hero.svelte";
-  import ShineBorder from "../components/ShineBorder.svelte";
 </script>
-
-<ShineBorder color="white">
-  Testing Shine Border
-</ShineBorder>
 
 <Hero />
