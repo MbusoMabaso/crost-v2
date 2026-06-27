3c139a1101a1c5dc640d0f6596bcfae725c0873d
Crost Dev <dev@crost.com>
2026-06-27 23:42:06 +0200

    feat: re-add ShineBorder to Home.svelte for testing

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,10 @@
 <script>
   import Hero from "../components/Hero.svelte";
+  import ShineBorder from "../components/ShineBorder.svelte";
 </script>
+
+<ShineBorder color="white">
+  Testing Shine Border
+</ShineBorder>
 
 <Hero />
